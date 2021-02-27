5b319de5bd20c1048883b7c90dcd0c4486763963
wardahgf <55796214+wardahgf@users.noreply.github.com>
2021-02-27 17:40:41 +0700

    week 2 dan 3

---
+++ WEEK 2/New Unity Project/Assets/Scenes/MenuActions.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuActions : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void MENU_ACTION_GoToPage(string sceneName)
+    {
+        Application.LoadLevel(sceneName);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ WEEK 3/Mini-Game Sederhana 2D-SpaceGirl(5)/Assets/Script/PlayerInventoryDisplay.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PlayerInventoryDisplay : MonoBehaviour
+{
+    public Image[] starPlaceholders;
+    public Sprite iconLove;
+    public Sprite iconStar;
+    public void OnChangeStarTotal(int starTotal)
+    {
+        for (int i = 0; i < starPlaceholders.Length; ++i)
+        {
+            if (i < starTotal)
+                starPlaceholders[i].sprite = iconStar;
+            else
+                starPlaceholders[i].sprite = iconLove;
+        }
+    }
+}
